8b5f00dd885256851bcaee7ff44f14e384b1b60e
shonmathew19 <shonformal@gmail.com>
2024-08-22 12:47:55 +0530

    initial commit

---
+++ src/redux/counterSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+//to initialize the file as slice we use a method createSlice()
+export const counterSlice = createSlice({
+    //name has no importance
+    name: 'counterApp',
+    //initial state creation
+    initialState: {
+        value: 0
+    },
+    reducers: {
+        //logic to update the value
+        // actions are created inside the reducers
+        increment: (state,data) => {
+            if(!state.value){
+                state.value = state.value + 1
+            }
+            state.value = state.value + data.payload
+        },
+        decrement: (state,data) => {
+            state.value -= data.payload
+        },
+        reset: (state) => {
+            state.value = 0;
+        }
+    }
+})
+
+export const {increment,decrement,reset}=counterSlice.actions
+export default counterSlice.reducer
